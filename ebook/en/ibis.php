a9c7ee923bb78bc539f6aa0195f0889108802206
bobbyiliev <bobby@bobbyiliev.com>
2025-10-17 20:19:46 +0300

    Switch to ibis-next

--- ebook/en/ibis.php
+++ ebook/en/ibis.php
@@ -12,6 +12,21 @@ return [
     'author' => 'Bobby Iliev',
 
     /**
+     * CSS inline style for the page header.
+     * If you want to skip header, comment the line
+     */
+    'header' => 'font-style: italic; text-align: right; border-bottom: solid 1px #808080;',
+
+    /**
+     * Cover page image configuration.
+     */
+    'cover' => [
+        'position' => 'position: absolute; left:0; right: 0; top: -.2; bottom: 0;',
+        'dimensions' => 'width: 210mm; height: 297mm; margin: 0;',
+        'image' => 'cover.jpg',
+    ],
+
+    /**
      * The list of fonts to be used in the different themes.
      */
     'fonts' => [
@@ -30,5 +45,5 @@ return [
      * A notice printed at the final page of a generated sample.
      */
     'sample_notice' => 'This is a sample from "Introduction to SQL" by Bobby Iliev. <br>
-                        For more information, <a href="https://devdojo.com/">Click here</a>.',
+                        For more information, <a href="https://bobbyiliev.com/">Click here</a>.',
 ];
